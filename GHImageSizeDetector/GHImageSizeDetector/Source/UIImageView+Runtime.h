461040f682ba1d7ecae1886e0c607d5df34b6b28
LiaoWorking <liaoworking@gmail.com>
2020-07-04 13:18:36 +0800

    delete unused code

--- GHImageSizeDetector/GHImageSizeDetector/Source/UIImageView+Runtime.h
+++ GHImageSizeDetector/GHImageSizeDetector/Source/UIImageView+Runtime.h
@@ -11,6 +11,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIImageView (Runtime)
+
+/// show the memory cost in your imageView.
 +(void)showMemoryCost;
 
 @end
--- GHImageSizeDetector/GHImageSizeDetector/Source/UIImageView+Runtime.m
+++ GHImageSizeDetector/GHImageSizeDetector/Source/UIImageView+Runtime.m
@@ -12,33 +12,37 @@
 
 
 +(void)showMemoryCost {
-    Method method1 = class_getInstanceMethod([self class], @selector(setImage:));
-    Method method2 = class_getInstanceMethod([self class], @selector(swizzled_setImage:));
-    //交换method1和method2的IMP指针，(IMP代表了方法的具体的实现）
-    method_exchangeImplementations(method1, method2);
+    Method originalMethod = class_getInstanceMethod([self class], @selector(setImage:));
+    Method swizzledMethod = class_getInstanceMethod([self class], @selector(swizzled_setImage:));
+    
+    BOOL didAddMethod = class_addMethod([self class], @selector(setImage:), method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod([self class], @selector(swizzled_setImage:), method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
 }
 
 
 - (void)swizzled_setImage:(UIImage *)image
 {
-    // call original implementation
     [self swizzled_setImage:image];
-        CATextLayer *imageSizeLayer = [[CATextLayer alloc]init];
-        imageSizeLayer.fontSize = 8;
-        imageSizeLayer.contentsScale = [UIScreen mainScreen].scale;
-        imageSizeLayer.backgroundColor = UIColor.blackColor.CGColor;
-        if (self.frame.size.width != 0) {
-            imageSizeLayer.frame = CGRectMake((self.frame.size.width - 30) * 0.5, (self.frame.size.height - 10) * 0.5, 30, 10);
-        } else {
-            imageSizeLayer.frame = CGRectMake(0, 0, 30, 10);
-        }
-        imageSizeLayer.string = [self stringWithbytes:[self calculateMemorySize:image]];
-        [self.layer addSublayer:imageSizeLayer];
-
-
+    
+    CATextLayer *imageSizeLayer = [[CATextLayer alloc]init];
+    imageSizeLayer.fontSize = 8;
+    imageSizeLayer.contentsScale = [UIScreen mainScreen].scale;
+    imageSizeLayer.backgroundColor = UIColor.blackColor.CGColor;
+    if (self.frame.size.width != 0) {
+        imageSizeLayer.frame = CGRectMake((self.frame.size.width - 30) * 0.5, (self.frame.size.height - 10) * 0.5, 30, 10);
+    } else {
+        imageSizeLayer.frame = CGRectMake(0, 0, 30, 10);
+    }
+    imageSizeLayer.string = [self stringWithbytes:[self calculateMemorySize:image]];
+    [self.layer addSublayer:imageSizeLayer];
 }
 
-/// Calculate the Memory Size Cost
+/// Calculate the Memory Size Cost pixel * 4 (r, g, b, alphe) * frameCount
 /// @param image image
 - (int)calculateMemorySize:(UIImage *)image {
     CGImageRef imageRef = image.CGImage;
@@ -52,7 +56,7 @@
     return (int)cost;
 }
 
-/// friendly Memory display
+/// friendly Memory usage display
 /// @param bytes bytes
 - (NSString *)stringWithbytes:(int)bytes {
   if (bytes < 1024) { // B
@@ -63,26 +67,4 @@
       return [NSString stringWithFormat:@"%.1fMB", (double)bytes / (1024 * 1024)];
   }
 }
-
-
-void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector)
-{
-    // the method might not exist in the class, but in its superclass
-    Method originalMethod = class_getInstanceMethod(class, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-    
-    // class_addMethod will fail if original method already exists
-    BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-    
-    // the method doesn’t exist and we just added one
-    if (didAddMethod) {
-        class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-    }
-    else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-    }
-    
-}
-
-
 @end
